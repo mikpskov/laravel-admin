603605b56257fce1568c13af4699f57b4475b50d
Mikhail Chilikin <mikpskov@gmail.com>
2022-04-21 00:02:48 +0300

    add tailwindcss package

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/admin/app.js', 'public/js/admin.js')
     .sass('resources/sass/admin/app.scss', 'public/css/admin.css')
-    .sourceMaps();
+    .sourceMaps()
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
