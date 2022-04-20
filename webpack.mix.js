570c3de1719ddc6623f6e3bd83ca6fe5f377a118
Mikhail Chilikin <mikpskov@gmail.com>
2022-04-20 23:49:44 +0300

    move admin assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/admin/app.js', 'public/js/admin.js')
+    .sass('resources/sass/admin/app.scss', 'public/css/admin.css')
     .sourceMaps();
