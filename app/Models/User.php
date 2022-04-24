622f1a2766dfb7e0c43befe184af863d7df81a1b
Mikhail Chilikin <mikpskov@gmail.com>
2022-04-25 01:58:18 +0300

    fix user post relation column

--- app/Models/User.php
+++ app/Models/User.php
@@ -74,7 +74,7 @@ class User extends Authenticatable
 
     public function posts(): HasMany
     {
-        return $this->hasMany(Post::class);
+        return $this->hasMany(Post::class, 'author_id');
     }
 
     public function getActions(): array
