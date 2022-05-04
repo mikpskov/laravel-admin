2292b8d3698ebcae7e0bd2e1e94707c9030599a6
Mikhail Chilikin <mikpskov@gmail.com>
2022-05-04 15:57:24 +0300

    add unique index for slug of tags

--- database/migrations/2022_04_27_224926_create_tags_tables.php
+++ database/migrations/2022_04_27_224926_create_tags_tables.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->string('slug', 255);
+            $table->string('slug', 255)->unique();
             $table->integer('order')->nullable();
             $table->timestamps();
         });
